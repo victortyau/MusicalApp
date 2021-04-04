10029a11e954306c126f5de49a9b529d57d4de35
Victor Tejada Yau <victortyau@gmail.com>
2021-04-03 19:55:03 -0500

    labeling activities

--- app/src/main/java/labs/bamboo/musicalapp/MainActivity.java
+++ app/src/main/java/labs/bamboo/musicalapp/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -19,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle(R.string.play_list);
         initialize();
     }
 
--- app/src/main/java/labs/bamboo/musicalapp/PlayMusicActivity.java
+++ app/src/main/java/labs/bamboo/musicalapp/PlayMusicActivity.java
@@ -17,7 +17,6 @@ public class PlayMusicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
-        setTitle(R.string.now_playing);
         initialize();
     }
 
